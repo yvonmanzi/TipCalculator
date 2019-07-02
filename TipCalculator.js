a22b897e18c8ba5377fa10039909931bb6ff1a35
yvonmanzi <46822938+yvonmanzi@users.noreply.github.com>
2019-07-01 21:36:13 -0400

    Update TipCalculator.js
    This is able to create the layout of the app the way I want it, but it's not computing the tip yet.

--- TipCalculator.js
+++ TipCalculator.js
@@ -1,7 +1,7 @@
-import React, {Component, propType} from 'react';
-import Form from './Form.js';
+import React, {propType, Component} from 'react';
+//import Form from './Form.js';
 
-class TipCalculator extends React{
+class TipCalculator extends React.Component{
     constructor(props){
         super(props);
 
@@ -10,51 +10,49 @@ class TipCalculator extends React{
             amount: null
         };
 
-        //binding 'this' to the methods
+        //binding 'this'
         this.calculate = this.calculate.bind(this);
-        this.handleNameInput = this.handleNameInput.bind(this);
         this.handleAmountInput = this.handleAmountInput.bind(this);
+        this.handleNameInput = this.handleNameInput.bind(this);
     }
     handleNameInput(event){
         this.setState({
             name: event.target.value,
-            amount: ''
         });
     }
     handleAmountInput(event){
         this.setState({
-             name: this.name,
-             amount: event.target.value
+            amount: event.target.value
         });
     }
     
     calculate(){
-        const tip = parseInt(this.state.props.amount)*0.2;// it would be kool to pass this % as a prop
+        var tip = parseInt(this.state.amount)*0.2;// it would be kool to pass this % as a prop
         return tip;
     }
-    
 render(){
 
     return(
         <div>
             <h1> Tipping should be fun</h1>
 
-            <p> Your Name: <input value={this.state.name} onChange = {this.handleNameInput} ></input></p>
+            <p> Your Name: <input value={this.state.name} onChange = {this.handleNameInput} /> </p>
 
-            <p> Bill: <input value={this.state.amount} onChange = {this.handleAmountInput} ></input></p>
+            <p> Bill: <input value={this.state.amount} onChange = {this.handleAmountInput} /> </p>
             <hr/>
             <p> Hi {this.state.name}! Your bill is: {this.state.amount}</p>
             <hr/>
             <button type = "button" onClick= {this.calculate}> Calculate </button>
-        
-            <input type="input">{this.calculate}</input>
+
+            <p> Here is your tip: <input type="input" value= {this.calculate} /> </p>
         </div>
 
     );
 }
 }
-TipCalculator.propType = {
-    name: propType.string.isRequired,
-    amount: propType.number.isRequired
-};
+
+//TipCalculator.propType = {
+    //name: propType.String.isRequired,
+   // amount: propType.number.isRequired
+//};
 export default TipCalculator;
